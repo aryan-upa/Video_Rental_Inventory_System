8404d7f65a8495a22d525248370c14308b869764
Aryan Upadhyay <aryan.offc@gmail.com>
2022-01-02 21:11:18 +0530

    add fields for the Video.java

--- src/definitions/Video.java
+++ src/definitions/Video.java
@@ -1,4 +1,15 @@
 package definitions;
 
 public class Video {
+
+    // Creating Definition Classes
+    // Rules:
+    // 1. Always set the visibility modifier of fields to private unless otherwise specified.
+    // 2. Always set the access modifier of fields to non-static unless otherwise mentioned.
+
+    // fields
+    private String videoName;
+    private int rating;
+    private boolean checkOut;
+
 }
