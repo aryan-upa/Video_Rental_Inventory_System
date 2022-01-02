5e4071ae74fddd081a79e15de89c213bd5a4211f
Aryan Upadhyay <aryan.offc@gmail.com>
2022-01-02 23:24:38 +0530

    add mutator methods for the Video.java fields.

--- src/definitions/Video.java
+++ src/definitions/Video.java
@@ -14,11 +14,50 @@ public class Video {
         return videoName;
     }
 
+    /**
+     * This method returns the Rating of the video.
+     *
+     * @return Video Rating
+     */
     public int getRating() {
         return rating;
     }
 
+    /**
+     * This method sets the rating of the video.
+     *
+     * @param rating : The rating of the video.
+     */
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    /**
+     * This method returns the checkOut status.
+     *
+     * @return checkOut Status.
+     */
     public boolean isCheckOut() {
         return checkOut;
     }
+
+    /**
+     * This method the Checkout Status of the video.
+     *
+     * @param checkOut : The checkout status of the video.
+     */
+    public void setCheckOut(boolean checkOut) {
+        this.checkOut = checkOut;
+    }
+
+    /**
+     * This method sets the Video Name of the file.
+     *
+     * @param videoName : The name of the video.
+     */
+    public void setVideoName(String videoName) {
+        this.videoName = videoName;
+    }
+
+
 }
