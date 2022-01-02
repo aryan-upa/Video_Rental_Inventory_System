561da296a500fb930c39a17dad8e3b10e67a0c19
Aryan Upadhyay <aryan.offc@gmail.com>
2022-01-02 23:25:28 +0530

    print message for the video name, rating and checkOut status.

--- src/execution/Main.java
+++ src/execution/Main.java
@@ -5,5 +5,12 @@ import definitions.Video;
 public class Main {
     public static void main(String[] args) {
         Video video = new Video();
+        System.out.println(
+                "Video name : " + video.getVideoName()
+                        + " has Rating : " + video.getRating()
+                        + ", is video available : " + video.isCheckOut()
+        );
+
+
     }
 }
