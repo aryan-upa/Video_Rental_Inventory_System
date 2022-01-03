4c8d1a43a961107bef85eee6000591458f65e73d
Aryan Upadhyay <aryan.offc@gmail.com>
2022-01-03 20:57:44 +0530

    create components method in the VideoStore.java file.

--- src/definitions/VideoStore.java
+++ src/definitions/VideoStore.java
@@ -1,10 +1,47 @@
 package definitions;
 
+import java.util.Arrays;
+
 public class VideoStore {
 
     /**
      * An array to store the videos object.
      */
     private Video[] store;
+
+    // Constructor
+    public VideoStore() {
+        this.store = new Video[5];
+    }
+
+    // Getter
+    public Video[] getStore() {
+        return store.clone();
+    }
+
+    // Setter
+    public void setStore(Video[] store) {
+        this.store = store;
+    }
+
+    // toString()
+    @Override
+    public String toString() {
+        return Arrays.toString(store);
+    }
+
+    // equals() and hashcode()
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        VideoStore that = (VideoStore) o;
+        return Arrays.equals(getStore(), that.getStore());
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(getStore());
+    }
 
 }
