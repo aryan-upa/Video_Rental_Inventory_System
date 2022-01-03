d4bd8dc9592b2b4b4965a2b16d868a5a122b94bb
Aryan Upadhyay <aryan.offc@gmail.com>
2022-01-03 22:22:51 +0530

    create switch for menu and static values in VideoLauncher.java for switch statements.

--- src/execution/VideoLauncher.java
+++ src/execution/VideoLauncher.java
@@ -1,11 +1,22 @@
 package execution;
 
+import definitions.VideoStore;
+
 import java.util.Scanner;
 
 public class VideoLauncher {
+
+    private static final int ADD_VIDEO = 1;
+    private static final int CHECK_OUT_VIDEO = 2;
+    private static final int RETURN_VIDEO = 3;
+    private static final int RECEIVE_RATING = 4;
+    private static final int LIST_INVENTORY = 5;
+    private static final int EXIT = 6;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int customerInput;
+        VideoStore myVideoStore = new VideoStore();
         do {
             System.out.println("MAIN MENU");
             System.out.println("=========");
@@ -17,6 +28,18 @@ public class VideoLauncher {
             System.out.println("6. Exit : ");
             System.out.println("Enter your choice (1..6) : ");
             customerInput = sc.nextInt();
+            switch (customerInput) {
+                case ADD_VIDEO:
+                    break;
+                case CHECK_OUT_VIDEO:
+                    break;
+                case RETURN_VIDEO:
+                    break;
+                case RECEIVE_RATING:
+                    break;
+                case LIST_INVENTORY:
+                    break;
+            }
         } while (customerInput != 6);
         sc.close();
 
